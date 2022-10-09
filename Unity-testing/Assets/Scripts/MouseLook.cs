e13f2710a3c915d415c5f85caed9b480cbc396ee
Viacheslav Gerasimenko <supursoul@gmail.com>
2022-10-09 20:19:01 +0300

    Add FP look script

---
+++ Unity-testing/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private Transform _playerBody;
+    [Header("Mouse Sensitivity")]
+    [SerializeField] private float _horizontalSensitivity = 100f;
+    [SerializeField] private float _verticalSensitivity = 150f;
+    [Header("Camera Settings")]
+    [SerializeField] private float _minVerticalRotation = 0f;
+    [SerializeField] private float _maxVerticalRotation = 180f;
+
+    private float xRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _horizontalSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _verticalSensitivity * Time.deltaTime;
+
+        _playerBody.Rotate(Vector3.forward * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, _minVerticalRotation, _maxVerticalRotation);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
